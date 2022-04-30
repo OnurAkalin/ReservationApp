6a9351a3d954df9d84e9486957414c68819f4991
Onur Akalın <onurakalin96@gmail.com>
2022-04-30 23:49:15 +0300

    context change

--- API/Controllers/UserController.cs
+++ API/Controllers/UserController.cs
@@ -6,7 +6,6 @@ namespace API.Controllers;
 [Route("api/[controller]/[action]")]
 public class UserController : ControllerBase
 {
-    private readonly IHttpContextAccessor _httpContextAccessor;
     private readonly ILogger<UserController> _logger;
     private readonly Guid _currentUserGuid;
     private readonly Guid _currentUserSelectedSiteId;
@@ -14,20 +13,21 @@ public class UserController : ControllerBase
     public UserController(ILogger<UserController> logger, IHttpContextAccessor httpContextAccessor)
     {
         _logger = logger;
-        _httpContextAccessor = httpContextAccessor;
 
         try
         {
-            if (_httpContextAccessor.HttpContext == null) return;
-            var siteId = _httpContextAccessor.HttpContext.Request.Headers["SiteId"];
+            if (httpContextAccessor.HttpContext == null) return;
+            
+            var siteId = httpContextAccessor.HttpContext.Request.Headers["SiteId"];
+            
             if (!string.IsNullOrEmpty(siteId))
             {
                 _currentUserSelectedSiteId = Guid.Parse(siteId);
             }
         }
-        catch (Exception ex)
+        catch (Exception)
         {
-            //_logger.Error(ex.ToString(), "{message}", "Header SiteId is null");
+            // ignored
         }
     }
     
