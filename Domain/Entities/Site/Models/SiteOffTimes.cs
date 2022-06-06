581ef4d687c607b786fa827df462f3bf1eb5e479
Onur Akalın <onurakalin96@gmail.com>
2022-06-07 02:34:01 +0300

    Site off time entity added.

---
+++ Domain/Entities/Site/Models/SiteOffTimes.cs
@@ -0,0 +1,9 @@
+﻿namespace Domain.Entities;
+
+public class SiteOffTimes : EntityBaseSite<int>
+{
+    public bool IsFullDay { get; set; }
+
+    public DateTime StartDate { get; set; }
+    public DateTime? EndDate { get; set; }
+}
--- Domain/Entities/Site/Models/SiteServiceDay.cs
+++ Domain/Entities/Site/Models/SiteServiceDay.cs
@@ -6,7 +6,6 @@ public class SiteServiceDay : EntityBase<int>
 {
     public Day Day { get; set; }
     public bool IsHoliday { get; set; }
-    public DateTime? Date { get; set; }
 
     [ForeignKey("SiteService")] public int SiteServiceId { get; set; }
     public SiteService SiteService { get; set; }
